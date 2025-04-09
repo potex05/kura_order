372e8822f0a2d8a970e81e812b822b1d5293a49d
potex05 <potetopoteto05@gmail.com>
2025-04-09 17:43:03 +0900

    Create data.js

---
+++ script/data.js
@@ -0,0 +1,19 @@
+const menuData = {
+  categories: [
+    { id: "sushi", name: "寿司" },
+    { id: "side", name: "サイド" },
+    { id: "dessert", name: "デザート" }
+  ],
+  items: {
+    sushi: [
+      { name: "まぐろ", img: "maguro.jpg", price: 130 },
+      { name: "サーモン", img: "salmon.jpg", price: 130 }
+    ],
+    side: [
+      { name: "特製茶碗蒸し", img: "chawanmushi.jpg", price: 260 }
+    ],
+    dessert: [
+      { name: "感動のぷりん", img: "pudding.jpg", price: 270 }
+    ]
+  }
+};
